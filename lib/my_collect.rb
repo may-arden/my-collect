a9bfc6a37993c06b71913dddb5d26047026f74a7
may arden <mayard3n@gmail.com>
2020-10-27 14:33:39 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
 
 
+def my_collect(collection)
+    empty_array = []
+    if block_given? 
+    i = 0
+      while i < collection.length
+        empty_array << (yield (collection[i]))
+        i = i + 1
+      end  
+    empty_array 
+    else   
+    collection 
+    end
+end
+
+
+
+
+
+
+
+
+
+
